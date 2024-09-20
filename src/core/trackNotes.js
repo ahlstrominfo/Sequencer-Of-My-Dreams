b57d022bd5a8e4fc09ccb85726c74444ab46de71
Mattias Ahlström <mattias.ahlstrom@vkmedia.se>
2024-09-20 16:40:46 +0200

    sdafvgahjkavbfjhkdsaf

--- src/core/trackNotes.js
+++ src/core/trackNotes.js
@@ -3,22 +3,29 @@ const { generateChord, conformNoteToScale } = require('../utils/scales');
 class TrackNotes {
     constructor(track) {
         this.track = track;
+        this.ticker = this.track.sequencer.ticker;
         this.currentNoteSeriesStep = 0;
 
     }
 
     scheduleNotes({startPulse, maxDuration, defaultDuration, currentTriggerStep}) {
-        
         if (this.checkTrackProbability()) {
+            const { bar, beat } = this.ticker.getPositionFromPulse(startPulse);
             const trackSettings = this.track.settings;
-            const noteSettings = trackSettings.noteSeries[this.currentNoteSeriesStep];
+            let noteSettings = trackSettings.noteSeries[this.currentNoteSeriesStep];
             if (trackSettings.useMaxDuration) {
                 defaultDuration = trackSettings.maxDurationFactor * maxDuration;
             }
 
-            
+            if (trackSettings.tieNoteSeriestoPattern) {
+                noteSettings = trackSettings.noteSeries[currentTriggerStep % trackSettings.noteSeries.length];
+            }
 
-            this.track.sequencer.logger.log(`Track: ${this.track.trackId} pulse: ${startPulse} maxD: ${maxDuration} smallest: ${defaultDuration} noteSeriesStep: ${noteSettings.rootNote} currentTriggerStep: ${currentTriggerStep}`);     
+            
+                      
+
+            // this.track.sequencer.logger.log(`Track: ${this.track.trackId} pulse: ${startPulse} maxD: ${maxDuration} smallest: ${defaultDuration} noteSeriesStep: ${noteSettings.rootNote} currentTriggerStep: ${currentTriggerStep} position: ${.bar}:${this.ticker.getPositionFromPulse(startPulse).beat}`);        
+            this.track.sequencer.logger.log(`${JSON.stringify(this.chordMakerForProgression(noteSettings, bar, beat))} ${startPulse} ${bar}:${beat}`);
         }
 
         this.updateCurrentNoteSeriesStep();
@@ -54,7 +61,7 @@ class TrackNotes {
     }    
 
     getProgressionInfo(bar, beat) {
-        return this.sequencer.getProgressionAtPosition(bar, beat);
+        return this.track.sequencer.getProgressionAtPosition(bar, beat);
     }    
 }
 
